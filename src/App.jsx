eae0bd8e4a0129d60d2d597a380ae643c4d958da
suhailsola <suhailsola@hotmail.com>
2023-08-15 14:12:01 +0800

    push

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Register from "./pages/Register";
 const App = () => {
   const router = createBrowserRouter([
     {
-      path: "/home",
+      path: "/",
       element: <Home />,
     },
     {
